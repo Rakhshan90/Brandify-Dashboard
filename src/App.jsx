635e00dbc743b8e92039f69a8020f8a482e78056
Rakhshan <rakhshancoder@gmail.com>
2023-04-15 15:47:47 +0530

    login page done with backend api

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import NewUser from "./Pages/NewUser";
 import ProductList from "./Pages/ProductList";
 import Product from "./Pages/Product";
 import NewProduct from "./Pages/NewProduct";
+import Login from "./Pages/Login";
+// import { useSelector } from "react-redux";
 
 
 const Container = styled.div`
@@ -21,24 +23,29 @@ margin-top: 10px;
 
 function App() {
 
+  // const admin = useSelector((state) => state.user.currentUser.isAdmin);
+
   return (
 
+
     <Router>
-      <Topbar />
-      <Container>
-        <Sidebar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/users" element={<UserList />} />
-          <Route path="/user/:userId" element={<User />} />
-          <Route path="/newUser" element={<NewUser />} />
-          <Route path="/products" element={<ProductList />} />
-          <Route path="/product/:productId" element={<Product />} />
-          <Route path="/newProduct" element={<NewProduct />} />
-        </Routes>
-      </Container>
+        <Topbar />
+        <Container>
+          <Sidebar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/users" element={<UserList />} />
+            <Route path="/user/:userId" element={<User />} />
+            <Route path="/newUser" element={<NewUser />} />
+            <Route path="/products" element={<ProductList />} />
+            <Route path="/product/:productId" element={<Product />} />
+            <Route path="/newProduct" element={<NewProduct />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Container>
     </Router>
   )
 }
 
 export default App
+
